01e69c004d94bdbabf9265eecf929fda800e3ac1
Vraja Das <vraja.pro@gmail.com>
2022-10-12 17:50:35 -0400

    refactor: Header
    * classes according to naming conventions
    * added class
    * avoided DRY by setting the images in array

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import styles from "../shopApp.module.css";
+import logo from "../images/droppe-logo.png";
+import img1 from "../images/img1.png";
+import img2 from "../images/img2.png";
 
-type Props = {}
+const Header = () => {
 
-const Header = (props: Props) => {
+    const images = [{src:img1,alt:"img1"},{src:img2,alt:"img2"}];
+
   return (
-    <div className={styles.header}>
-    <div className={['container', styles.headerImageWrapper].join(' ')}>
-      <img src={logo} className={styles.headerImage} />
+    <>
+     <div className={styles.header}>
+            <div className={`container ${styles["logo-img-wrapper"]}`}>
+            <img src={logo} className={styles['logo-img']} alt="Logo"/>
+            </div>
     </div>
-  </div>
+    <div className={`container ${styles.main} ${styles["header-img-wrapper"]}`}>
+    {images.map((img,i)=> <img src={img.src} className={styles['header-img']} alt={img.alt} key={`header-img-${i}`}/>)}
+           
+    </div>
+    </>
+   
   )
 }
 
