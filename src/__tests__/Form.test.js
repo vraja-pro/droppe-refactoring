db0db6a3ca305ccf502647d5cab0eab4963aa97c
Vraja Das <vraja.pro@gmail.com>
2022-10-14 22:10:51 -0400

    fix: undo merge of tests branch

---
+++ src/__tests__/Form.test.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {render,screen, cleanup, fireEvent} from '@testing-library/react'
+import Form from '../components/form';
+import renderer from 'react-test-renderer'
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import Modal from "react-modal"
 import "./index.css";
-import ShopApp from "./ShopApp";
+import { ShopApp } from "./shop-app";
 import * as serviceWorker from "./serviceWorker";
 
 Modal.setAppElement("#root")
