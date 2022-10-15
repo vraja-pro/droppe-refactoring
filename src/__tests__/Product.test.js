171afe78a197b636bd4475dc66f2678dd837f042
Vraja Das <vraja.pro@gmail.com>
2022-10-15 19:26:04 -0400

    test: refactor product fav unit test

--- src/__tests__/Product.test.js
+++ src/__tests__/Product.test.js
@@ -12,20 +12,16 @@ const productProps = {id:'5',title:'product title', description:'product descrip
 
 
 test('should render not favorite in Product component', () => { 
-    render(<Product {...productProps}/>);
-    const productElement = screen.getByTestId('product-1');
+    render(<Product isFavorite={false}/>);
+    const productElement = screen.getByText('Add to favorites');
     expect(productElement).toBeInTheDocument();
-    expect(productElement).toHaveTextContent('Add to favorites');
     
 })
 
 test('should render favorite in Product component', () => { 
-    const productProps = {id:'2',title:'product title', description:'product description',price:'100',isFavorite:true,onFav}
-    render(<Product {...productProps}/>);
-    const productElement = screen.getByTestId('product-2');
+    render(<Product isFavorite={true}/>);
+    const productElement = screen.getByText('Remove from favorites');
     expect(productElement).toBeInTheDocument();
-    expect(productElement).toHaveTextContent('Remove from favorites');
-
 })
 
 test('product card matches snapshot', () => { 
