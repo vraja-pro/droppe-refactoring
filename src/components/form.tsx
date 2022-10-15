221be29a69fd0637c59948c8bbcefc34e030a161
Vraja Das <vraja.pro@gmail.com>
2022-10-15 13:30:06 -0400

    test: testing if onSubmit funciton is called

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -32,19 +32,22 @@ export const Form: FC<FormProps> = ({onSubmit}) => {
 
       <input
         name="title"
+        data-testid="title"
         placeholder="Title..."
         className={styles.input}
       />
 
-      <label className={styles.label}>Product details: *</label>
+      <label className={styles.label} htmlFor="description">Product details: *</label>
 
       <input
+        data-testid="price"
         name="price"
         placeholder="Price..."
         className={styles.input}
       />
 
       <textarea
+        data-testid="description"
         name="description"
         placeholder="Start typing product description here..."
         className={styles.textarea}
